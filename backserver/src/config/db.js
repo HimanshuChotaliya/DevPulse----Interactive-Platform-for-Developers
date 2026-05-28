3a99695da586a06232153eafd6d81a2e59ea8e5e
Himanshu Chotaliya <himanshu746h@gmail.com>
2026-05-28 11:09:08 +0530

    chore: fix preflight cors and conditional ssl handling

--- backserver/src/config/db.js
+++ backserver/src/config/db.js
@@ -10,7 +10,7 @@ const pool = new Pool(
     process.env.DATABASE_URL
         ? {
             connectionString: process.env.DATABASE_URL,
-            ssl: isInternalRailway ? false : { rejectUnauthorized: false }
+            ssl: { rejectUnauthorized: false }
         }
         : {
             host: process.env.DB_HOST || "localhost",
