b398140a648172417043592eb638ecbdbf7d157e
Himanshu Chotaliya <himanshu746h@gmail.com>
2026-05-28 12:00:24 +0530

    chore: implement axios token interceptor for secure cross-origin requests

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,9 +1,19 @@
-// client/src/api/axios.js
 import axios from "axios"
+import useAuthStore from "../store/authStore"
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL.replace(/\/api\/?$/, "") : "",
   withCredentials: true
 })
+
+api.interceptors.request.use((config) => {
+  const token = useAuthStore.getState().token;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
 
 export default api
